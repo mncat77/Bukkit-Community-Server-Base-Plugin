11d833c0b309c7ab6b6be72a61f3b1c349541871
mncat77 <jo-97@web.de>
2013-01-21 21:44:11 +0100

    Update Bukkit Community Server/src/com/puffybugs/BasePlugin/BasePlugin.java
    added reference to Listener ( I have that trying to create that now :D )

--- Bukkit Community Server/src/com/puffybugs/BasePlugin/BasePlugin.java
+++ Bukkit Community Server/src/com/puffybugs/BasePlugin/BasePlugin.java
@@ -1,5 +1,17 @@
-package com.puffybugs.BasePlugin;
+package com.puffybugs.baseplugin;
 
-public class BasePlugin {
+import com.puffybugs.baseplugin.listeners.PlayerJoinListener;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class BasePlugin extends JavaPlugin implements Listener {
+    
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+    }
 }
